131cb202a21ee41e7d4aeee10ae4944498b1bd66
Jonathan Jefferies <jonathan.jefferies@supergroup.co.uk>
2013-10-09 16:12:00 +0100

    Added deleting of empty directories.

--- src/jjok/Decomposer/Console/Command/KeepCommand.php
+++ src/jjok/Decomposer/Console/Command/KeepCommand.php
@@ -2,6 +2,7 @@
 
 namespace Decomposer\Console\Command;
 
+use Decomposer\Finder\Adapter\ChildFirstPhpAdapter;
 use Symfony\Component\Console\Command\Command;
 // use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -31,27 +32,32 @@ class KeepCommand extends Command {
 	
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
+		//TODO Do this somewhere else. Add a Config class
 		$xml = new \DOMDocument('1.0', 'UTF-8');
-		$xml->load('../decomposer.dist.xml');
+		$xml->load('decomposer.dist.xml');
 
 		foreach($xml->getElementsByTagName('keep') as $keep) {
 
+			# Get the start directory
 			$start = $keep->getAttribute('start');
+			$output->writeln('Cleaning up '.$start);
 			
+			# Get the paths to keep
 			$paths = array();
 			foreach($keep->getElementsByTagName('path') as $path) {
-// 				$paths[] = $path->nodeValue;
 				$paths[] = sprintf('/%s/', str_replace('/', '\/', $path->nodeValue));
 			}
 
-			$finder = new Finder();
-			$finder->addAdapter(new \Decomposer\Finder\Adapter\ChildFirstPhpAdapter())
+			# Create the finder
+			$finder = Finder::create();
+			$finder->addAdapter(new ChildFirstPhpAdapter())
 			       ->setAdapter('child-first')
 			       ->ignoreVCS(false)
 			       ->ignoreDotFiles(false)
 			       ->ignoreUnreadableDirs(true)
 			       ->in($start);
 			
+			# Add paths to keep
 			foreach($paths as $path) {
 // 				$path = sprintf('/%s/', str_replace('/', '\/', $path));
 				// 	$finder->path($path);
@@ -62,24 +68,27 @@ class KeepCommand extends Command {
 				// 	print $file->getRelativePathname()."\n";
 // 				printf('Deleting %s%s', $file->getRelativePathname(), PHP_EOL);
 			
+				# Delete all files
 				if($file->isFile()) {
 					$output->writeln('Deleting '. $file->getPathname());
 // 					unlink($file->getPathname());
-					
 				}
+				# Delete any empty directories
 				else{
-// 					print_r($file);
-// 				exit();
-// 				// 	print_r($file->getChildren());
-// 					if(!$file->hasChildren()) {
-// 						printf('Deleting %s%s', $file->getPathname(), PHP_EOL);
-// // 						rmdir($file->getPathname());
-// 					}
-// 					else {
-// 						printf('Keeping %s%s', $file->getPathname(), PHP_EOL);
-// 					}
+					//TODO Do this better
+					foreach(scandir($file) as $i) {
+						if(!in_array($i, array('.', '..'))) {
+							$output->writeln('Keeping '. $file->getPathname());
+							continue 2;
+						}
+					}
+// 					print_r(scandir($file));
+					$output->writeln('Deleting '. $file->getPathname());
+// 					rmdir($file->getPathname());
 				}
 			}
+			
+			$output->writeln('Finished.');
 		}
 	}
 }
