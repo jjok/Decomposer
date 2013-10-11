11ddff0e88b485c998c85773db6aee751b9dbb1c
Jonathan Jefferies <jonathan.jefferies@supergroup.co.uk>
2013-10-11 17:28:31 +0100

    Allow for multiple commands.

--- README.md
+++ README.md
@@ -3,6 +3,17 @@ Decomposer
 
 Deletes all the junk out of your project.
 
+Usage
+------
+
+Keep only the files listed in the Decomposer config.
+
+	php path/to/decomposer.phar keep
+
+See which files will be removed.
+
+	php path/to/decomposer.phar keep -d -v
+
 TODO
 ----
 
--- bin/decomposer.php
+++ bin/decomposer.php
@@ -2,5 +2,7 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$application = new jjok\Decomposer\Decomposer();
+// $application = new jjok\Decomposer\Decomposer('Decomposer', '0.1.0');
+$application = new Symfony\Component\Console\Application('Decomposer', '0.1.0');
+$application->add(new jjok\Decomposer\Console\Command\KeepCommand());
 $application->run();
