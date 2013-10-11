cfbb4286b60ccaaae08890b8b7949fbe3e304ed9
Jonathan Jefferies <jonathan.jefferies@supergroup.co.uk>
2013-10-11 17:27:59 +0100

    Added options to `keep` command.

--- src/jjok/Decomposer/Console/Command/KeepCommand.php
+++ src/jjok/Decomposer/Console/Command/KeepCommand.php
@@ -4,33 +4,22 @@ namespace jjok\Decomposer\Console\Command;
 
 use jjok\Decomposer\Finder\Adapter\ChildFirstPhpAdapter;
 use Symfony\Component\Console\Command\Command;
-// use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-// use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Console\Output\Output;
 
 class KeepCommand extends Command {
-	protected function configure()
-	{
+	
+	protected function configure() {
 		$this->setName('keep')
 		     ->setDescription('Keeps files.')
-// 		->addArgument(
-// 				'name',
-// 				InputArgument::OPTIONAL,
-// 				'Who do you want to greet?'
-// 		)
-// 		->addOption(
-// 				'yell',
-// 				null,
-// 				InputOption::VALUE_NONE,
-// 				'If set, the task will yell in uppercase letters'
-// 		)
-		;
+		     ->addOption('config', 'c', InputOption::VALUE_REQUIRED, '')
+		     ->addOption('dry-run', 'd', InputOption::VALUE_NONE, '');
 	}
 	
-	protected function execute(InputInterface $input, OutputInterface $output)
+	protected function execute(InputInterface $input, OutputInterface $output) 
 	{
 		//TODO Do this somewhere else. Add a Config class
 		$xml = new \DOMDocument('1.0', 'UTF-8');
@@ -59,36 +48,48 @@ class KeepCommand extends Command {
 			
 			# Add paths to keep
 			foreach($paths as $path) {
-// 				$path = sprintf('/%s/', str_replace('/', '\/', $path));
-				// 	$finder->path($path);
+// 				$finder->path($path);
 				$finder->notPath($path);
 			}
 			
-			foreach ($finder as $file) {
-				// 	print $file->getRelativePathname()."\n";
-// 				printf('Deleting %s%s', $file->getRelativePathname(), PHP_EOL);
-			
-				# Delete all files
-				if($file->isFile()) {
-					$output->writeln('Deleting '. $file->getPathname());
-// 					unlink($file->getPathname());
-				}
-				# Delete any empty directories
-				else{
-					//TODO Do this better
-					foreach(scandir($file) as $i) {
-						if(!in_array($i, array('.', '..'))) {
-							$output->writeln('Keeping '. $file->getPathname());
-							continue 2;
-						}
-					}
-// 					print_r(scandir($file));
-					$output->writeln('Deleting '. $file->getPathname());
-// 					rmdir($file->getPathname());
-				}
-			}
+			$this->delete($finder, $output, true);
+// 			$this->delete($finder, $output, $input->getOption('dry-run'));
 			
 			$output->writeln('Finished.');
+		}
+	}
+	
+	/**
+	 * 
+	 * @param Finder $finder
+	 * @param OutputInterface $output
+	 * @param boolean $dry_run
+	 */
+	private function delete(Finder $finder, OutputInterface $output, $dry_run) {
+		foreach ($finder as $file) {
+				
+			# Delete all files
+			if($file->isFile()) {
+				$output->writeln('Deleting '. $file->getPathname(), Output::VERBOSITY_VERBOSE);
+				if(!$dry_run) {
+					unlink($file->getPathname());
+				}
+			}
+			# Delete any empty directories
+			else {
+				//TODO Do this better
+				# Keep non-empty directories
+				foreach(scandir($file) as $i) {
+					if(!in_array($i, array('.', '..'))) {
+						$output->writeln('Keeping '. $file->getPathname(), Output::VERBOSITY_VERBOSE);
+						continue 2;
+					}
+				}
+				$output->writeln('Deleting '. $file->getPathname(), Output::VERBOSITY_VERBOSE);
+				if(!$dry_run) {
+					rmdir($file->getPathname());
+				}
+			}
 		}
 	}
 }
