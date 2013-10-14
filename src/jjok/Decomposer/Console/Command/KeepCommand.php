378999b3e9b6daa6b39d87b705cf5d772eb253c8
Jonathan Jefferies <jonathan.jefferies@supergroup.co.uk>
2013-10-14 16:59:08 +0100

    Using Factory in KeepCommand.

--- src/jjok/Decomposer/Console/Command/KeepCommand.php
+++ src/jjok/Decomposer/Console/Command/KeepCommand.php
@@ -2,7 +2,7 @@
 
 namespace jjok\Decomposer\Console\Command;
 
-use jjok\Decomposer\Finder\Adapter\ChildFirstPhpAdapter;
+use jjok\Decomposer\Factory;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -12,40 +12,41 @@ use Symfony\Component\Console\Output\Output;
 
 class KeepCommand extends Command {
 	
+	/**
+	 * (non-PHPdoc)
+	 * @see \Symfony\Component\Console\Command\Command::configure()
+	 */
 	protected function configure() {
 		$this->setName('keep')
-		     ->setDescription('Keeps files.')
+		     ->setDescription('Removes files as specified in config file.')
 		     ->addOption('config', 'c', InputOption::VALUE_REQUIRED, '')
 		     ->addOption('dry-run', 'd', InputOption::VALUE_NONE, '');
 	}
 	
+	/**
+	 * (non-PHPdoc)
+	 * @see \Symfony\Component\Console\Command\Command::execute()
+	 */
 	protected function execute(InputInterface $input, OutputInterface $output) 
 	{
-// 		$xml = new \DOMDocument('1.0', 'UTF-8');
-// 		$xml->load('decomposer.dist.xml');
-// 		$paths_to_keep = $xml->getElementsByTagName('keep');
 		$config = $this->getApplication()->getConfig();
 
+		if(!$input->getOption('dry-run')) {
+			$dialog = $this->getHelperSet()->get('dialog');
+			if (!$dialog->askConfirmation(
+					$output,
+					'<question>Continue with this action? (y|n)</question>',
+					false
+				)) {
+				return;
+			}
+		}
+		
 		foreach($config->getPathsToKeep() as $paths) {
 
-			# Get the start directory
-// 			$start = $keep->getAttribute('start');
-// 			$output->writeln('Cleaning up '.$start);
-			
-			# Get the paths to keep
-// 			$paths = array();
-// 			foreach($keep->getElementsByTagName('path') as $path) {
-// 				$paths[] = sprintf('/%s/', str_replace('/', '\/', $path->nodeValue));
-// 			}
-
 			# Create the finder
-			$finder = Finder::create();
-			$finder->addAdapter(new ChildFirstPhpAdapter())
-			       ->setAdapter('child-first')
-			       ->ignoreVCS(false)
-			       ->ignoreDotFiles(false)
-			       ->ignoreUnreadableDirs(true)
-			       ->in($paths->getStart());
+			$finder = Factory::createFinder()
+					->in($paths->getStart());
 			
 			# Add paths to keep
 			foreach($paths->getPaths() as $path) {
@@ -57,8 +58,43 @@ class KeepCommand extends Command {
 			$this->deleteAll($finder, $output, $input->getOption('dry-run'));
 		}
 		
+// 		foreach($config->getPathsToRemove() as $paths) {
+		
+// 			# Create the finder
+// 			$finder = Factory::createFinder()
+// 					->in($paths->getStart());
+			
+// 			# Add paths to remove
+// 			foreach($paths->getPaths() as $path) {
+// 				$finder->path($path->toRegEx());
+// 			}
+			
+// 			$this->deleteAll($finder, $output, $input->getOption('dry-run'));
+// 		}
+		
 		$output->writeln('Finished.');
 	}
+	
+// 	private function something(array $paths_list, $remove, OutputInterface $output) {
+// 		foreach($paths_list as $paths) {
+		
+// 			# Create the finder
+// 			$finder = Factory::createFinder()
+// 					->in($paths->getStart());
+				
+// 			# Add paths to remove
+// 			foreach($paths->getPaths() as $path) {
+// 				if($remove) {
+// 					$finder->path($path->toRegEx());
+// 				}
+// 				else {
+// 					$finder->notPath($path->toRegEx());
+// 				}
+// 			}
+			
+// 			$this->deleteAll($finder, $output, $input->getOption('dry-run'));
+// 		}
+// 	}
 	
 	/**
 	 * Recursively delete all files found by the given Finder.
@@ -67,28 +103,35 @@ class KeepCommand extends Command {
 	 * @param boolean $dry_run
 	 */
 	private function deleteAll(Finder $finder, OutputInterface $output, $dry_run) {
-		foreach ($finder as $file) {
+		foreach($finder as $file) {
 				
 			# Delete all files
 			if($file->isFile()) {
-				$output->writeln('Deleting '. $file->getPathname(), Output::VERBOSITY_VERBOSE);
+				if($output->getVerbosity() === Output::VERBOSITY_VERBOSE) {
+					$output->writeln('Deleting '. $file->getPathname(), Output::VERBOSITY_VERBOSE);
+				}
 				if(!$dry_run) {
 					unlink($file->getPathname());
 				}
 				continue;
 			}
 			
-			# Delete any empty directories
 // 			else {
 			//TODO Do this better
 			# Keep non-empty directories
 			foreach(scandir($file) as $i) {
 				if(!in_array($i, array('.', '..'))) {
-					$output->writeln('Keeping '. $file->getPathname(), Output::VERBOSITY_VERBOSE);
+					if($output->getVerbosity() === Output::VERBOSITY_VERBOSE) {
+						$output->writeln('Keeping '. $file->getPathname());
+					}
 					continue 2;
 				}
 			}
-			$output->writeln('Deleting '. $file->getPathname(), Output::VERBOSITY_VERBOSE);
+
+			# Delete any empty directories
+			if($output->getVerbosity() === Output::VERBOSITY_VERBOSE) {
+				$output->writeln('Deleting '. $file->getPathname());
+			}
 			if(!$dry_run) {
 				rmdir($file->getPathname());
 			}
--- src/jjok/Decomposer/Decomposer.php
+++ src/jjok/Decomposer/Decomposer.php
@@ -2,22 +2,33 @@
 
 namespace jjok\Decomposer;
 
-use jjok\Decomposer\Console\Command\KeepCommand;
+use jjok\Decomposer\Config\Config;
 use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\InputInterface;
 
 class Decomposer extends Application
 {
 	
+	/**
+	 * The application configuration.
+	 * @var \jjok\Decomposer\Config\Config
+	 */
 	protected $config;
 	
-	public function __construct($config, $version) {
+	/**
+	 * Set the application configuration.
+	 * @param \jjok\Decomposer\Config\Config $config
+	 * @param string $version
+	 */
+	public function __construct(Config $config, $version) {
 		$this->config = $config;
 		
 		parent::__construct('Decomposer', $version);
 	}
 	
-	
+	/**
+	 * Get the application configuration.
+	 * @return \jjok\Decomposer\Config\Config
+	 */
 	public function getConfig() {
 		return $this->config;
 	}
