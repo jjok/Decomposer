e8f95beac0607b57d7f998bca8b962de614447ed
Jonathan Jefferies <jonathan.jefferies@supergroup.co.uk>
2013-10-15 17:09:46 +0100

    Added `try` and `catch` around main application.

--- bin/decomposer.php
+++ bin/decomposer.php
@@ -2,11 +2,17 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-# Load config file
-$loader = new jjok\Decomposer\Config\Loader();
-$config = $loader->load();
-
-# Run application
-$application = new jjok\Decomposer\Decomposer($config, '0.1.0');
-$application->add(new jjok\Decomposer\Console\Command\KeepCommand());
-$application->run();
+try {
+	# Load config file
+	$loader = new jjok\Decomposer\Config\Loader();
+	$config = $loader->load();
+	
+	# Run application
+	$application = new jjok\Decomposer\Decomposer($config, '0.1.0');
+	$application->add(new jjok\Decomposer\Console\Command\KeepCommand());
+	$application->run();
+}
+catch(Exception $e) {
+	echo $e->getMessage();
+	exit(1);
+}
