d6e835266bf238386d0271ba571fb882bceee334
Jonathan Jefferies <jonathan.jefferies@supergroup.co.uk>
2013-11-27 12:43:38 +0000

    Added unit test for `Path`.

--- tests/jjok/Decomposer/Config/PathTest.php
+++ tests/jjok/Decomposer/Config/PathTest.php
@@ -6,7 +6,26 @@ use PHPUnit_Framework_TestCase;
 
 class PathTest extends PHPUnit_Framework_TestCase {
 	
-	public function testTodo() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+	/**
+	 * @expectedException PHPUnit_Framework_Error
+	 */
+	public function testPathNameIsRequired() {
+		$path = new Path();
+	}
+	
+	public function testPathNameCanBeSet() {
+		$this->assertSame('some-path', (string) new Path('some-path'));
+		$this->assertSame('some-file.php', (string) new Path('some-file.php'));
+	}
+	
+	public function testPathCanBeConvertedToRegEx() {
+		$path = new Path('some-path');
+		$this->assertSame('/^some-path/', $path->toRegEx());
+		
+		$path = new Path('some-path/some-dir/some-file.php');
+		$this->assertSame('/^some-path\/some-dir\/some-file.php/', $path->toRegEx());
+		
+		$path = new Path('some-path\some-dir\some-file.php');
+		$this->assertSame('/^some-path\some-dir\some-file.php/', $path->toRegEx());
 	}
 }
