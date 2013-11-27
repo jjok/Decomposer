0bd8e40c947a645ad3d2feeb212bb12e49450d50
Jonathan Jefferies <jonathan.jefferies@supergroup.co.uk>
2013-11-27 13:04:34 +0000

    Added `Paths` unit test.

--- tests/jjok/Decomposer/Config/PathsTest.php
+++ tests/jjok/Decomposer/Config/PathsTest.php
@@ -6,7 +6,48 @@ use PHPUnit_Framework_TestCase;
 
 class PathsTest extends PHPUnit_Framework_TestCase {
 	
-	public function testTodo() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+	/**
+	 * @expectedException PHPUnit_Framework_Error
+	 */
+	public function testStartIsRequired() {
+		$paths = new Paths();
+	}
+	
+	public function testStartCanBeSet() {
+		$paths = new Paths('my-start');
+		$this->assertSame('my-start', $paths->getStart());
+		$this->assertCount(0, $paths->getPaths());
+	}
+	
+	public function testPathsCanBeSet() {
+		$paths = new Paths('another-start', array(
+			'dummy-path',
+			'dummy-path2'
+		));
+		$this->assertCount(2, $paths->getPaths());
+	}
+	
+	public function testPathsCanBeAdded() {
+		$paths = new Paths('');
+		$this->assertCount(0, $paths->getPaths());
+		
+		$paths->addPath(new Path(''));
+		$this->assertCount(1, $paths->getPaths());
+		
+		$paths->addPath(new Path(''));
+		$this->assertCount(2, $paths->getPaths());
+	}
+	
+	public function testAdditionalPathsCanBeAdded() {
+		$paths = new Paths('', array(
+			'', ''
+		));
+		$this->assertCount(2, $paths->getPaths());
+	
+		$paths->addPath(new Path(''));
+		$this->assertCount(3, $paths->getPaths());
+	
+		$paths->addPath(new Path(''));
+		$this->assertCount(4, $paths->getPaths());
 	}
 }
