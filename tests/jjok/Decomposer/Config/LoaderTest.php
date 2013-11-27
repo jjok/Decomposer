c917147cf870bcd8673e16a29e480cd2f4c3e28d
Jonathan Jefferies <jonathan.jefferies@supergroup.co.uk>
2013-11-27 12:35:09 +0000

    Added unit test for `Loader`.

--- tests/jjok/Decomposer/Config/LoaderTest.php
+++ tests/jjok/Decomposer/Config/LoaderTest.php
@@ -6,7 +6,30 @@ use PHPUnit_Framework_TestCase;
 
 class LoaderTest extends PHPUnit_Framework_TestCase {
 	
+	/**
+	 * @covers jjok\Decomposer\Config\Loader::load
+	 */
 	public function testTodo() {
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		$loader = new Loader();
+		$config = $loader->load(__DIR__.'/../../../dummies/dummy.xml');
+		
+		$this->assertInstanceOf('jjok\Decomposer\Config\Config', $config);
+		$this->assertCount(1, $config->getPathsToKeep());
+		
+		$config = $loader->load(__DIR__.'/../../../dummies/dummy2.xml');
+		
+		$this->assertInstanceOf('jjok\Decomposer\Config\Config', $config);
+		
+		$paths_to_keep = $config->getPathsToKeep();
+		$this->assertCount(3, $paths_to_keep);
+
+		$this->assertSame('some-start-path', $paths_to_keep[0]->getStart());
+		$this->assertCount(1, $paths_to_keep[0]->getPaths());
+		
+		$this->assertSame('some-start-path2', $paths_to_keep[1]->getStart());
+		$this->assertCount(2, $paths_to_keep[1]->getPaths());
+		
+		$this->assertSame('some-start-path3', $paths_to_keep[2]->getStart());
+		$this->assertCount(4, $paths_to_keep[2]->getPaths());
 	}
 }
