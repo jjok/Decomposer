ceeb9d80d4a5aeef29ad4a6a1125693775fb9339
Jonathan Jefferies <jonathan.jefferies@supergroup.co.uk>
2013-11-27 12:16:38 +0000

    Added empty unit tests for all Config classes.

---
+++ tests/jjok/Decomposer/Config/FinderTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace jjok\Decomposer\Config;
+
+use PHPUnit_Framework_TestCase;
+
+class FinderTest extends PHPUnit_Framework_TestCase {
+	
+	/**
+	 * @expectedException PHPUnit_Framework_Error
+	 */
+	public function testFileNameIsRequired() {
+		$finder = new Finder();
+	}
+
+	/**
+	 * @expectedException PHPUnit_Framework_Error
+	 */
+	public function testFileTypeIsRequired() {
+		$finder = new Finder('some-name');
+	}
+	
+	/**
+	 * @expectedException jjok\Decomposer\Config\MissingConfigException
+	 */
+	public function testExceptionThrownIfNoConfigFileFound() {
+		$finder = new Finder('test', 'xml');
+		$finder->find();
+	}
+	
+	public function testDist() {
+		touch('test.dist.xml');
+		$finder = new Finder('test', 'xml');
+		$this->assertSame('test.dist.xml', $finder->find());
+		unlink('test.dist.xml');
+	}
+	
+	public function testLocalConfigHasPriorityOverDist() {
+		touch('test.dist.xml');
+		touch('test.xml');
+		$finder = new Finder('test', 'xml');
+		$this->assertSame('test.xml', $finder->find());
+		unlink('test.dist.xml');
+		unlink('test.xml');
+	}
+}
--- tests/jjok/Decomposer/Config/PathsTest.php
+++ tests/jjok/Decomposer/Config/PathsTest.php
@@ -4,7 +4,7 @@ namespace jjok\Decomposer\Config;
 
 use PHPUnit_Framework_TestCase;
 
-class LoaderTest extends PHPUnit_Framework_TestCase {
+class PathsTest extends PHPUnit_Framework_TestCase {
 	
 	public function testTodo() {
 		$this->markTestIncomplete('This test has not been implemented yet.');
